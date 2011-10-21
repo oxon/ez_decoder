ff11f8ff68c3be22376bc0a9e6c53fdd3584ddaa
Yves Senn <yves.senn@garaio.com>
2011-10-21 13:58:08 +0200

    Die Kontonummer korrekt aus den roten ES für Bank lesen

--- lib/ez_decoder/roter_einzahlungsschein.rb
+++ lib/ez_decoder/roter_einzahlungsschein.rb
@@ -9,21 +9,37 @@ module EzDecoder
       :einzahlungsschein_rot
     end
 
+    def postzahlung?
+      @kodierzeile.length == 10
+    end
+
+    def bankzahlung?
+      !postzahlung?
+    end
+
     def konto_nummer
+      if bankzahlung?
+        konto_nummer_sektion[0..-2]
+      else
+        konto_nummer_sektion
+      end
+    end
+
+    def konto_nummer_sektion
       if @kodierzeile =~ /(\d+)(?:\+|>)/
         $~[1]
       end
     end
 
-    def bankenclearing
+    def bankenclearing_sektion
       if @kodierzeile =~ /\+\s(\d+)>/
         $~[1]
       end
     end
 
     def bankenclearing_nummer
-      return nil unless bankenclearing
-      bankenclearing[2, 5]
+      return nil unless bankenclearing_sektion
+      bankenclearing_sektion[2, 5]
     end
   end
 end
--- spec/ez_decoder/roter_einzahlungsschein_spec.rb
+++ spec/ez_decoder/roter_einzahlungsschein_spec.rb
@@ -8,18 +8,22 @@ describe EzDecoder::RoterEinzahlungsschein do
   context "Bei Bankzahlungen" do
     let(:kodierzeile) { '000000000000000000234512342+ 070888845>' }
 
+    it { should be_bankzahlung }
+    it { should_not be_postzahlung }
+
     its(:art) { should == :einzahlungsschein_rot }
-    its(:konto_nummer) { should == '000000000000000000234512342' }
-    its(:bankenclearing) { should == '070888845' }
+    its(:konto_nummer) { should == '00000000000000000023451234' }
     its(:bankenclearing_nummer) { should == '08888' }
   end
 
   context "Bei Postzahlungen" do
     let(:kodierzeile) { '250090342>' }
 
+    it { should be_postzahlung }
+    it { should_not be_bankzahlung }
+
     its(:art) { should == :einzahlungsschein_rot }
     its(:konto_nummer) { should == '250090342' }
-    its(:bankenclearing) { should be_nil }
     its(:bankenclearing_nummer) { should be_nil }
   end
 
